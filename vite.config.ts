bb40bef7a05193c00001e852dcbc050467215fcf
AhmadOmari10 <adfa3566@gmail.com>
2026-07-23 16:55:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 👈 هذا هو السطر الجديد الذي أضفناه
   plugins: [react()],
   resolve: {
     alias: {
